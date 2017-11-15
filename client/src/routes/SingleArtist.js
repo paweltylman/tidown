afbc005d1a94d4c307b69999cf3a670cb266dad3
Spencer Charest <SpencerC77@gmail.com>
2017-11-15 13:19:31 -0800

    added avatar to artist page

--- client/src/routes/SingleArtist.js
+++ client/src/routes/SingleArtist.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
+import { Avatar, Divider } from 'react-md';
 import fetchArtistAlbums from '../actions/fetchArtistAlbums';
 import fetchArtistInfo from '../actions/fetchArtistInfo';
 import AlbumResults from '../containers/AlbumResults';
@@ -30,16 +31,27 @@ class SingleArtist extends Component {
       <div>
         <div style={{ marginTop: 40 }}>
           <div className="md-grid">
-            <h1 className="md-cell--12 md-text-center artist-header">
-              {artist.data.name}
-            </h1>
+            <div className="md-cell--12 md-text-center">
+              <Avatar
+                src={artist.data.picture.lg}
+                role="presentation"
+                className="avatar-lg"
+              />
+              <h1
+                className="artist-header"
+              >
+                {artist.data.name}
+              </h1>
+            </div>
           </div>
+          <Divider />
           <div className="md-grid" style={{ marginTop: 40 }}>
             <h1 className="md-cell--12">
               Top Tracks
             </h1>
           </div>
           <TopTracks tracks={artist.data.topTracks} album />
+          <Divider />
           <div className="md-grid" style={{ marginTop: 40 }}>
             <h1 className="md-cell--12">
               Albums
