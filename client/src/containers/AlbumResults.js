655bd885390d85a611f762926d7cfed34f7dd263
Spencer Charest <SpencerC77@gmail.com>
2017-11-18 10:43:00 -0800

    added title is only one album is shown

--- client/src/containers/AlbumResults.js
+++ client/src/containers/AlbumResults.js
@@ -41,12 +41,19 @@ class ArtistAlbums extends Component {
       }
 
       return (
-        <Album
-          album={isAvailable ? available[album.id] : album}
-          key={album.id}
-          available={isAvailable}
-          queued={isQueued}
-        />
+        <div>
+          {
+            albums.data.length === 1 ? (
+              <h1 className="md-text-center">{albums.data[0].title}</h1>
+            ) : null
+          }
+          <Album
+            album={isAvailable ? available[album.id] : album}
+            key={album.id}
+            available={isAvailable}
+            queued={isQueued}
+          />
+        </div>
       );
 
 
