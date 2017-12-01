0df92450c9927d95e728ec9228cb2c30c6da5545
Spencer Charest <SpencerC77@gmail.com>
2017-12-01 09:33:15 -0800

    Removed WebFontLoader

--- client/src/index.js
+++ client/src/index.js
@@ -2,22 +2,13 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import { Provider } from 'react-redux';
 import { ConnectedRouter } from 'react-router-redux';
-import WebFontLoader from 'webfontloader';
 import Routes from './routes';
 import store, { history } from './store';
 import './index.css';
 import registerServiceWorker from './registerServiceWorker';
 
-WebFontLoader.load({
-  google: {
-    families: ['Roboto:300,400,500,700', 'Material Icons'],
-  },
-});
-
 history.listen((location) => {
   window.scrollTo(0, 0);
-  store.dispatch({ type: 'CLEAR_ALBUMS' });
-  store.dispatch({ type: 'CLEAR_ARTIST' });
 });
 
 ReactDOM.render(
