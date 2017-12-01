a6ba73ef64a2da3c0519243e2bd9d3e612f6f7b9
Spencer Charest <SpencerC77@gmail.com>
2017-12-01 10:09:52 -0800

    Changed scanner trigger

--- helpers/scanner.js
+++ helpers/scanner.js
@@ -2,7 +2,7 @@ import { execSync } from 'child_process';
 import { fb } from '../app';
 
 const scanner = () => {
-  fb.ref('/artists').on('value', (snapshot) => {
+  fb.ref('/albums').on('child_added', (snapshot) => {
     if (process.env.POST_PROCESS) {
       execSync(process.env.POST_PROCESS);
     }
