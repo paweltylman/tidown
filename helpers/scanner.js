648891c17b5ffd43bac120204647ba22854cd86d
Spencer Charest <SpencerC77@gmail.com>
2017-12-02 19:24:11 -0800

    Fixed scanner

--- helpers/scanner.js
+++ helpers/scanner.js
@@ -2,8 +2,9 @@ import { execSync } from 'child_process';
 import { fb } from '../app';
 
 const scanner = () => {
-  fb.ref('/queue').on('child_removed', (snapshot) => {
-    if (!snapshot.val()) {
+  fb.ref('/queue').on('child_removed', async (snapshot) => {
+    const albums = await fb.ref('/queue/albums').once('value');
+    if (!albums.val()) {
       if (process.env.POST_PROCESS) {
         execSync(process.env.POST_PROCESS);
       }
